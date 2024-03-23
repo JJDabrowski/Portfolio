97a307ade5be1008a183452646fbba45e4c84079
JJDabrowski <jjrdabrowski@gmail.com>
2024-03-23 15:15:14 +0100

    adding azure-wise setting for deployment

--- Shroomcast/core/settings.py
+++ Shroomcast/core/settings.py
@@ -114,3 +114,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Azure settings
+SCM_DO_BUILD_DURING_DEPLOYMENT = 1
